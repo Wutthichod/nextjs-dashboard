0839881f45db927e0086d50b04142c4d5070e661
wutthichod <wutthichod@gmail.com>
2024-07-04 17:01:09 +0700

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Roboto, Lusitana } from 'next/font/google';
+
+export const roboto = Roboto({ subsets: ['latin'], weight: '300'});
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400'})
